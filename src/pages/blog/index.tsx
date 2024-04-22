12b128ba17624c2c4f80e98a2dade8766c454cf4
Daniel <danielbatista.thoughtworks@latam.com>
2024-04-22 10:06:00 -0300

    test hydration

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -7,7 +7,7 @@ export default function BlogPage({ people }: { people: any }) {
   return (
     <div style={{ width: '100%' }}>
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-       <h1>Pokemon characters </h1>
+       <h1>Pokemon characters v2 </h1>
         {people.results.map((person: any, index: number) => (
           <CardComponent
             key={person.name}
